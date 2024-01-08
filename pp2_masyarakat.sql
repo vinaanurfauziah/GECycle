b50e048228c1f99c8bc13578852487743eb57b43
Vina Nur Fauziah <@vinaanurfauziah@gmail.com>
2024-01-08 14:41:13 +0700

    menambahkan database

--- pp2_masyarakat.sql
+++ pp2_masyarakat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 04, 2024 at 04:06 AM
+-- Generation Time: Jan 08, 2024 at 07:39 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -29,13 +29,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `masyarakat` (
   `id` int NOT NULL,
-  `foto` varchar(15) NOT NULL,
   `nama` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `username` varchar(20) NOT NULL,
   `alamat` varchar(150) NOT NULL,
-  `tanggal_lahir` date NOT NULL
+  `tanggal_lahir` date NOT NULL,
+  `no_telp` char(13) NOT NULL,
+  `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `masyarakat`
+--
+
+INSERT INTO `masyarakat` (`id`, `nama`, `username`, `alamat`, `tanggal_lahir`, `no_telp`, `password`) VALUES
+(1, 'asep', 'asep', 'jl.bengong', '2024-01-09', '089999112234', '12345');
 
 -- --------------------------------------------------------
 
@@ -57,7 +64,8 @@ CREATE TABLE `sampah` (
 -- Indexes for table `masyarakat`
 --
 ALTER TABLE `masyarakat`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `sampah`
@@ -73,7 +81,7 @@ ALTER TABLE `sampah`
 -- AUTO_INCREMENT for table `masyarakat`
 --
 ALTER TABLE `masyarakat`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `sampah`
